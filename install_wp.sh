39f17150838e4e04b82d59f574a90d01ad489131
Gábor Angyal <angyalgabor@t-online.hu>
2020-12-04 14:56:16 +0100

    Update install_wp.sh

--- install_wp.sh
+++ install_wp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update
 apt upgrade
-apt install apache2 mariadb-server php libapache2-mod-php php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
+apt install apache2 mariadb-server php libapache2-mod-php php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip git unzip
 mysql -u root -e 'CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;'
 mysql -u root -e "GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'password';"
 mysql -u root -e 'FLUSH PRIVILEGES;'
@@ -26,3 +26,6 @@ git clone https://github.com/agabor/wpconfig.git
 mv wpconfig/.git .
 mv wpconfig/wp-config.php .
 rm -rf wpconfig
+cd /tmp
+sudo wget https://codesharp.hu/custom-nginxlight.zip
+unzip custom-nginxlight.zip
